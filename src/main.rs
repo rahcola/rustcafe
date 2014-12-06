326ca79d959b419ef3062c891c160709d176ba5f
Jani Rahkola <jani.rahkola@iki.fi>
2014-12-06 14:29:29 +0200

    docopt and uber api function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-#![feature(slicing_syntax)]
-extern crate serialize;
+#![feature(slicing_syntax, phase)]
+extern crate docopt;
+#[phase(plugin)]extern crate docopt_macros;
 extern crate hyper;
+extern crate serialize;
 
-use serialize::json;
-use hyper::Url;
-use hyper::status::StatusCode;
+use docopt::Docopt;
 use hyper::client::Request;
 use hyper::client::Response;
+use hyper::status::StatusCode;
+use hyper::Url;
+use serialize::json;
 
 #[deriving(Decodable, Show)]
 struct ApiResponse<T> {
@@ -31,38 +34,61 @@ struct Menu {
     data: Vec<Food>,
 }
 
-fn api(url_str: &str) -> Response {
-    let url = match Url::parse(url_str) {
-        Ok(u) => u,
-        Err(e) => panic!("bad url {}: {}", url_str, e),
-    };
+fn api<T: serialize::Decodable<serialize::json::Decoder,
+                               serialize::json::DecoderError>>
+    (url_str: &str) -> T {
+    let url = Url::parse(url_str).unwrap();
     let res = match Request::get(url)
-        .and_then(|r| { r.start() })
-        .and_then(|r| { r.send() }) {
-            Ok(r @ Response {status: StatusCode::Ok, ..})
-                => r,
+        .and_then(|r| r.start())
+        .and_then(|r| r.send()) {
+            Ok(ref mut r @ Response {status: StatusCode::Ok, ..})
+                => r.read_to_string().unwrap(),
             Ok(Response {status: x, ..})
                 => panic!("GET {} failed: {}", url_str, x),
             Err(e)
                 => panic!("GET {} failed: {}", url_str, e),
         };
-    res
+    let r: ApiResponse<T> = json::decode(res[]).unwrap();
+    r.data
 }
 
 fn restaurants() -> Vec<Restaurant> {
-    let url = "http://messi.hyyravintolat.fi/publicapi/restaurants";
-    let res = api(url[]).read_to_string().unwrap();
-    let response: ApiResponse<Vec<Restaurant>> = json::decode(res[]).unwrap();
-    response.data
+    api("http://messi.hyyravintolat.fi/publicapi/restaurants")
 }
 
 fn menus(id: u64) -> Vec<Menu> {
-    let url = format!("http://messi.hyyravintolat.fi/publicapi/restaurant/{}", id);
-    let res = api(url[]).read_to_string().unwrap();
-    let response: ApiResponse<Vec<Menu>> = json::decode(res[]).unwrap();
-    response.data
+    api(format!("http://messi.hyyravintolat.fi/publicapi/restaurant/{}", id)[])
 }
 
+fn restaurant_id(rs: &Vec<Restaurant>, name: &str) -> Option<u64> {
+    for x in rs.iter() {
+        if x.name[] == name {
+            return Some(x.id)
+        }
+    }
+    None
+}
+
+docopt!(Args deriving Show, "
+Usage: rustcafe <restaurant>
+")
+
 fn main() {
-    println!("{}", menus(11));
+    let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
+    let rs = restaurants();
+    let r = args.arg_restaurant[];
+    match restaurant_id(&rs, r) {
+        Some(id) => for m in menus(id).iter() {
+            println!("{}", m.date);
+            for f in m.data.iter() {
+                println!("\t{}", f.name);
+            }
+        },
+        None => {
+            println!("no restaurant {} exists", r);
+            for r in rs.iter() {
+                println!("{}", r.name[]);
+            }
+        },
+    }
 }
