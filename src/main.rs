c43d06150a80a1fdafa0054f247c4abbe524fa61
Jani Rahkola <jani.rahkola@iki.fi>
2014-12-06 20:56:56 +0200

    --today

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 #![feature(slicing_syntax, phase)]
+extern crate core;
+extern crate chrono;
 extern crate docopt;
 #[phase(plugin)]extern crate docopt_macros;
 extern crate hyper;
+extern crate regex;
+#[phase(plugin)]extern crate regex_macros;
 extern crate serialize;
 
+use core::fmt::{Error, Formatter, Show};
+use chrono::{Date, UTC, FixedOffset, Datelike, Weekday};
 use docopt::Docopt;
 use hyper::client::Request;
 use hyper::client::Response;
 use hyper::status::StatusCode;
 use hyper::Url;
-use serialize::json;
+use serialize::{json, Decoder, Decodable};
 
 #[deriving(Decodable, Show)]
 struct ApiResponse<T> {
@@ -22,6 +28,8 @@ struct Restaurant {
     id: u64,
     name: String,
 }
+
+struct UnicafeDate(Date<FixedOffset>);
 
 #[deriving(Decodable, Show)]
 struct Price {
@@ -36,12 +44,46 @@ struct Food {
 
 #[deriving(Decodable, Show)]
 struct Menu {
-    date: String,
+    date: UnicafeDate,
     data: Vec<Food>,
 }
 
-fn api<T: serialize::Decodable<serialize::json::Decoder,
-                               serialize::json::DecoderError>>
+fn finnish_weekday(w: Weekday) -> &'static str {
+    match w {
+        Weekday::Mon => "Ma",
+        Weekday::Tue => "Ti",
+        Weekday::Wed => "Ke",
+        Weekday::Thu => "To",
+        Weekday::Fri => "Pe",
+        Weekday::Sat => "La",
+        Weekday::Sun => "Su",
+    }
+}
+
+impl Show for UnicafeDate {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        let UnicafeDate(d) = *self;
+        write!(f, "{} {}.{}", finnish_weekday(d.weekday()), d.day(), d.month())
+    }
+}
+
+impl<D: Decoder<E>, E> Decodable<D, E> for UnicafeDate {
+    fn decode(d: &mut D) -> Result<UnicafeDate, E> {
+        let now = unicafe_today();
+        let s = try!(d.read_str());
+        let cap = regex!(r"^[:alpha:]+ (\d{1,2})\.(\d{1,2})$")
+            .captures(s[]).unwrap();
+        let now = now.with_month(from_str(cap.at(2)).unwrap()).unwrap()
+            .with_day(from_str(cap.at(1)).unwrap()).unwrap();
+        Ok(UnicafeDate(now))
+    }
+}
+
+fn unicafe_today() -> Date<FixedOffset> {
+    UTC::today().with_offset(FixedOffset::east(60*60*2))
+}
+
+fn api<T: serialize::Decodable<json::Decoder, json::DecoderError>>
     (url_str: &str) -> T {
     let url = Url::parse(url_str).unwrap();
     let res = match Request::get(url)
@@ -88,7 +130,10 @@ fn price_symbol(food: &Food) -> &'static str {
 }
 
 docopt!(Args deriving Show, "
-Usage: rustcafe <restaurant>
+Usage: rustcafe [--today] <restaurant>
+
+Options:
+    --today  display only today's menu
 ")
 
 fn main() {
@@ -97,9 +142,19 @@ fn main() {
     let r = args.arg_restaurant[];
     match restaurant_id(&rs, r) {
         Some(id) => for m in menus(id).iter() {
-            println!("{}", m.date);
-            for f in m.data.iter() {
-                println!("\t{}\t{}", price_symbol(f), f.name);
+            if args.flag_today {
+                let UnicafeDate(date) = m.date;
+                if date == unicafe_today() {
+                    for f in m.data.iter() {
+                        println!("{}\t{}", price_symbol(f), f.name);
+                    }
+                    return;
+                }
+            } else {
+                println!("{}", m.date);
+                for f in m.data.iter() {
+                    println!("\t{}\t{}", price_symbol(f), f.name);
+                }
             }
         },
         None => {
