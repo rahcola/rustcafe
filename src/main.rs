a0669b716b17b6067c9b84c1aff4e86c9c4744fe
Jani Rahkola <jani.rahkola@iki.fi>
2014-12-06 13:04:34 +0200

    decoding menus using types

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,17 @@ struct Restaurant {
     name: String,
 }
 
+#[deriving(Decodable, Show)]
+struct Food {
+    name: String,
+}
+
+#[deriving(Decodable, Show)]
+struct Menu {
+    date: String,
+    data: Vec<Food>,
+}
+
 fn restaurants() -> Vec<Restaurant> {
     let url = Url::parse("http://messi.hyyravintolat.fi/publicapi/restaurants").unwrap();
     let res = Request::get(url)
@@ -26,26 +37,24 @@ fn restaurants() -> Vec<Restaurant> {
         .unwrap()
         .read_to_string()
         .unwrap();
-    let response: Response<Vec<Restaurant>> = json::decode(res.as_slice()).unwrap();
+    let response: Response<Vec<Restaurant>> = json::decode(res[]).unwrap();
     response.data
 }
 
-fn menus(id: u64) -> Vec<(String, Vec<String>)> {
-    let url = Url::parse(format!("http://messi.hyyravintolat.fi/publicapi/restaurant/{}", id)[]);
-    let req = Request::get(url.unwrap()).unwrap();
-    let response = req.start().unwrap().send().unwrap().read_to_string().unwrap();
-    let o = json::from_str(response.as_slice()).unwrap();
-    let menus = o.as_object().unwrap().get("data").unwrap().as_array().unwrap();
-    menus.iter().map(|menu| {
-        let date = menu.as_object().unwrap().get("date_en").unwrap().as_string().unwrap().to_string();
-        let foods = menu.as_object().unwrap().get("data")
-            .unwrap().as_array().unwrap().iter().map(|o| {
-                o.as_object().unwrap().get("name").unwrap().as_string().unwrap().to_string()
-            }).collect::<Vec<String>>();
-        (date, foods)
-    }).collect::<Vec<(String, Vec<String>)>>()
+fn menus(id: u64) -> Vec<Menu> {
+    let url = Url::parse(format!("http://messi.hyyravintolat.fi/publicapi/restaurant/{}", id)[]).unwrap();
+    let res = Request::get(url)
+        .and_then(|r| { r.start() })
+        .and_then(|r| { r.send() })
+        .unwrap()
+        .read_to_string()
+        .unwrap();
+    match json::decode(res[]) {
+        Ok(Response { data: d, .. }) => d,
+        Err(e) => {println!("{}", res[]); panic!(e)},
+    }
 }
 
 fn main() {
-    println!("{}", restaurants());
+    println!("{}", menus(11));
 }
